68de0998a227bc0d38a62786ee4f5c94572b67d0
Koen van Esterik <kd.vanesterik@student.han.nl>
2024-01-02 15:25:46 +0100

    [ci] resolve base url issue

--- build.py
+++ build.py
@@ -3,6 +3,8 @@ from flask_frozen import Freezer
 from app import create_app
 
 app = create_app()
+app.config["FREEZER_BASE_URL"] = "/student-journal/"
+
 freezer = Freezer(app)
 
 if __name__ == "__main__":
